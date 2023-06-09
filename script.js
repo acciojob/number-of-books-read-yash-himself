96c116348f12d431a65248dfcec0409a9802312a
Yash Gupta <yashgupta27690@gmail.com>
2023-06-09 16:24:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count=0;
+	for(int i=0;i<library.length;i++){
+		if (library[i].readingStatus) {
+			count++;
+		}
+	}
+	return count;
 };
 
 // Do not change the code below
